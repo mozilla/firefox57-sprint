75be726e0acea86bc8de35647b971c283ae1f446
Michael Kohler <me@michaelkohler.info>
2017-08-20 22:46:33 +0200

    Add leaderboard

---
+++ _articles/resources/leaderboard.md
@@ -0,0 +1,20 @@
+---
+layout: page
+title: "Leaderboard"
+---
+
+<table id="leaderboard" class="hidden">
+  <thead>
+    <tr>
+      <th>Nickname</th>
+      <th>Number of tested sites</th>
+    </tr>
+  </thead>
+  <tbody>
+  </tbody>
+</table>
+
+<p id="loading">Loading...</p>
+
+<script src='https://cdnjs.cloudflare.com/ajax/libs/tabletop.js/1.5.1/tabletop.min.js'></script>
+<script src="{{ site.baseurl }}/js/leaderboard.js"></script>
---
+++ js/leaderboard.js
@@ -0,0 +1,52 @@
+(function() {
+  'use strict';
+
+  var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1ddte9oFxtIIp9AzWe9oGwBIscokjvKoFx6WvAiQmznY/pubhtml';
+
+  var leaderboardTableBody = document.querySelector('#leaderboard tbody');
+
+  function initTabletop() {
+    Tabletop.init({
+      key: publicSpreadsheetUrl,
+      callback: processInfo,
+      simpleSheet: true
+    });
+  }
+
+  function processInfo(data, tabletop) {
+    var groupedByNickname = groupByNickname(data);
+    console.log(groupedByNickname);
+    Object.keys(groupedByNickname).forEach(function(name) {
+      var row = document.createElement('tr');
+
+      var nameCell = document.createElement('td');
+      nameCell.textContent = name;
+      row.appendChild(nameCell);
+
+      var totalCell = document.createElement('td');
+      totalCell.textContent = groupedByNickname[name];
+      row.appendChild(totalCell);
+
+      document.querySelector('#loading').classList.add('hidden');
+      leaderboardTableBody.appendChild(row);
+      document.querySelector('#leaderboard').classList.remove('hidden');
+    });
+  }
+
+  function groupByNickname(entries) {
+    console.log(entries);
+    var result = {};
+
+    entries.map(function(entry) {
+      if (result[entry.Nickname]) {
+        result[entry.Nickname]++;
+      } else {
+        result[entry.Nickname] = 1;
+      }
+    });
+
+    return result;
+  }
+
+  window.addEventListener('DOMContentLoaded', initTabletop);
+})();
