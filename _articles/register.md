1b514d790d75b0e2a090c03700debe7d522b591b
Michael Kohler <me@michaelkohler.info>
2017-09-14 23:11:42 +0200

    WIP: start to load events dynamically

--- _articles/register.md
+++ _articles/register.md
@@ -13,18 +13,6 @@ title: REGISTER
 <iframe width="100%" height="520" frameborder="0" src="https://www.google.com/maps/d/u/0/embed?mid=1AaL9Hv0VroENQOEjIY2aYPhCKic&zoom=14" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
 
 <div class="row map-sites">
-{% for country in site.data.events %}
-  <div class="col-lg-3 col-sm-4 col-xs-6">
-  <i class="mg mg-5x map-{{ country[1][0].locationCountryCode | downcase }}"></i>
-  <hr >
-  <h4>{{ country[0] }}</h4>
-  {% for event in country[1] %}
-    {% if event.signupLink %}
-      <div><a target="_blank" href="{{ event.signupLink }}">{{ event.locationName }}, {{ event.locationCity }}</a></div>
-    {% endif %}
-  {% endfor %}
-  </div>
-{% endfor %}
 </div>
 
 <script src="{{ site.baseurl }}/js/mapData.js"></script>
--- js/mapData.js
+++ js/mapData.js
@@ -6,13 +6,35 @@
   function initTabletop() {
     Tabletop.init({
       key: publicSpreadsheetUrl,
-      callback: drawMap,
+      callback: processData,
       simpleSheet: true
     });
   }
 
-  function drawMap(data) {
-    console.log('look, your data is here', data);
+  function processData(events) {
+    addEventList(events);
+    drawMap(events);
+  }
+
+  function drawMap(events) {
+    console.log('look, your events are here', events);
+  }
+
+  function addEventList(events) {
+    const container = document.querySelector('.map-sites');
+
+    /**{% for country in site.data.events %}
+    <div class="col-lg-3 col-sm-4 col-xs-6">
+    <i class="mg mg-5x map-{{ country[1][0].locationCountryCode | downcase }}"></i>
+    <hr >
+    <h4>{{ country[0] }}</h4>
+    {% for event in country[1] %}
+      {% if event.signupLink %}
+        <div><a target="_blank" href="{{ event.signupLink }}">{{ event.locationName }}, {{ event.locationCity }}</a></div>
+      {% endif %}
+    {% endfor %}
+    </div>
+  {% endfor %}*/
   }
 
   window.addEventListener('DOMContentLoaded', initTabletop);
